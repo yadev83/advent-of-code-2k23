41f4755c1d3308ea39c1d897a94a8531a076d177
Yanis <yanis.attia@outlook.fr>
2023-12-01 17:27:46 +0100

    fixed relative imports filesystem utils

--- src/utils/filesystem.js
+++ src/utils/filesystem.js
@@ -1,5 +1,9 @@
 import path from 'path'
+import url from 'url'
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
 export const getPath = (filePath) => {
-    return path.join('__dirname', filePath);
+    return path.join(__dirname, '../', filePath);
 }
